f4a37b7ec3345313033dd36ce4ae22723fc416cb
LewisARJ <lewisreyes07@gmail.com>
2026-06-13 01:23:38 -0400

    SOME CHanges made

--- src/GestorDeSolicitud/GestorDeSolicitud.API/obj/Debug/net10.0/GestorDeSolicitud.API.AssemblyInfo.cs
+++ src/GestorDeSolicitud/GestorDeSolicitud.API/obj/Debug/net10.0/GestorDeSolicitud.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestorDeSolicitud.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d4de740611be92b482c3ead479ddd1902964991")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cc38ac14012790ac7d940b1892ff984865566e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestorDeSolicitud.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestorDeSolicitud.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
